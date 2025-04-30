2bb975dcf569b7474bc46b26b46e89fbb2960570
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-30 15:40:46 +0530

    feature: Add submit button for game and show won/lost status on submit

--- src/scripts/components/Game.ts
+++ src/scripts/components/Game.ts
@@ -1,10 +1,12 @@
 import { gameData } from '../data';
+import { calculateGameAnswer } from '../utils/calculateGameAnswer';
 import GameBoard from './GameBoard';
 import GameHints from './GameHints';
 
 function Game(): HTMLElement {
     const gameContainer = document.createElement('main');
 
+    const gameResult: Array<Array<string>> = calculateGameAnswer(gameData);
     const userInput: Array<Array<string>> = [];
 
     for (let i = 0; i < 5; i++) {
@@ -15,6 +17,27 @@ function Game(): HTMLElement {
     }
 
     gameContainer.append(GameBoard(gameData, userInput));
+
+    const submitButton = document.createElement('button');
+    submitButton.textContent = 'Submit';
+    submitButton.className = 'submit-btn';
+    submitButton.addEventListener('click', () => {
+        let isCorrect = true;
+        outer: for (let i = 0; i < gameResult.length; i++) {
+            for (let j = 0; j < gameResult[i].length; j++) {
+                if (userInput[i][j] !== gameResult[i][j]) {
+                    isCorrect = false;
+                    break outer;
+                }
+            }
+        }
+
+        if (isCorrect) alert('Congratulations ! You WON this game.');
+        else alert('You answers are NOT correct.');
+    });
+
+    gameContainer.append(submitButton);
+
     gameContainer.append(GameHints(gameData));
 
     return gameContainer;
