064999d5a4edf3fc0d7d41ffe310ca7dcdd0a930
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-30 14:41:47 +0530

    feature: Add index and input field in enabled cells

--- src/scripts/components/GameBoard.ts
+++ src/scripts/components/GameBoard.ts
@@ -7,25 +7,53 @@ function GameBoard(gameData: Array<GameWord>): HTMLDivElement {
     const gameBoard = document.createElement('div');
     gameBoard.className = 'board';
 
+    const gameBoardCells: Array<Array<HTMLDivElement>> = [];
+
     for (let i = 0; i < 5; i++) {
         const gameRow = document.createElement('div');
         gameRow.className = 'board-row';
+
+        gameBoardCells.push([]);
 
         for (let j = 0; j < 6; j++) {
             const gameCell = document.createElement('div');
             gameCell.className = 'board-cell';
             gameRow.append(gameCell);
 
-            if (
-                disabledCells.findIndex(
-                    (item) => item.row - 1 === i && item.col - 1 === j
-                ) !== -1
-            ) {
-                gameCell.classList.add('disabled');
-            }
+            gameBoardCells[i].push(gameCell);
         }
 
         gameBoard.append(gameRow);
+    }
+
+    for (const disabledCell of disabledCells) {
+        gameBoardCells[disabledCell.row - 1][
+            disabledCell.col - 1
+        ].classList.add('disabled');
+    }
+
+    for (const gameWord of gameData) {
+        const gameBoardCell =
+            gameBoardCells[gameWord.row - 1][gameWord.col - 1];
+
+        const boardCellIndex = document.createElement('span');
+        boardCellIndex.textContent = `${gameWord.index}`;
+        boardCellIndex.className = 'board-cell-number';
+
+        gameBoardCell.append(boardCellIndex);
+    }
+
+    for (let i = 0; i < gameBoardCells.length; i++) {
+        for (const gameBoardCell of gameBoardCells[i]) {
+            if (!gameBoardCell.classList.contains('disabled')) {
+                const cellInput = document.createElement('input');
+                cellInput.type = 'text';
+                cellInput.className = 'board-cell-input';
+                cellInput.maxLength = 1;
+
+                gameBoardCell.append(cellInput);
+            }
+        }
     }
 
     return gameBoard;
